25673ed6345f2479ed9a6b3544cf687fc4585eb6
Sujay <12659871+apalesujay@users.noreply.github.com>
2018-05-11 01:00:27 +0530

    Basic Templates and Configuration Variables defined

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'Sujay'}
+    posts = [
+        {
+            'author' : {'username' : 'Aditya'},
+            'body' : 'Best Flask Tutorial'
+        },
+        {
+            'author' : {'username' : 'Jitendra'},
+            'body' : 'Learn Python in 24 Hrs'
+        }
+    ]
+    return render_template('index.html', title = 'Web Log', user = user, posts = posts)
